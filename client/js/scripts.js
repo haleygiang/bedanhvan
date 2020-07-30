04a9b7328f3b14793ba74c696b72f002d5f72805
Danny Phan <phanhaidang29102000@gmail.com>
2020-07-31 00:23:32 +0700

    add audio

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,9 +1,26 @@
-function getPronunication(text) {
+function getPronunication(id, text) {
     $.ajax({
         type: 'POST',
         url: 'http://localhost:3000/api',
         data: { data: text },
-        success:  response => console.log(response),
+        success:  response => {
+            console.log(response.async);
+            $("#" + id).html(`<audio controls class="d-none">
+                                <source src="${response.async}" type="audio/mpeg">
+                                Your browser does not support the audio element.
+                            </audio>`)
+        },
         failure: response => console.log(response)
     })
 }
+
+function toggleAudio(id) {
+    if ($("#" + id).find("audio").hasClass("d-none")) {
+        $("#" + id).find("audio").removeClass("d-none");
+    } else {
+        $("#" + id).find("audio").addClass("d-none");
+    }
+}
+
+getPronunication('audio-1', 'Cờ on con Gờ a ga huyền gà');
+getPronunication('audio-2', 'Cờ on con Bờ o bo huyền bò');
